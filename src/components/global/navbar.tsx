12915dc9fb5ef39ec6abaa55398330ae8885c746
Taylor Beck <beck.taylorg@gmail.com>
2024-04-19 10:58:15 -0700

    Added product section

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import React from 'react'
+import { MenuIcon } from 'lucide-react'
 
 type Props = {}
 
@@ -18,7 +19,7 @@ const Navbar = async (props: Props) => {
         />
         <p className="text-3xl font-bold">netic</p>
       </aside>
-      <nav className="absolute left-[55%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+      <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
         <ul className="flex items-center gap-4 list-none">
           <li>
             <Link href="#">Products</Link>
@@ -50,6 +51,7 @@ const Navbar = async (props: Props) => {
             {true ? 'Dashboard' : 'Get Started'}
           </span>
         </Link>
+        <MenuIcon className="md:hidden" />
       </aside>
     </header>
   )

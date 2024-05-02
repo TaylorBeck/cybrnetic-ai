feb91d334d4e65fc082bbc49c9badd57b398bb1b
Taylor Beck <beck.taylorg@gmail.com>
2024-05-01 18:14:37 -0700

    Added auth routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server"
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRoutes: ['/', '/api/clerk-webhook', '/api/drive-activity/notification'],
+  ignoredRoutes: [
+    '/api/auth/callback/discord',
+    '/api/auth/callback/notion',
+    '/api/auth/callback/slack',
+    '/api/flow',
+    '/api/cron/wait'
+  ]
+})
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"]
+}
